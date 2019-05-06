bff677c99cbc41edacece6a9ee83aab2678c4510
Matej Kardy Kardoš <matej.kardos@gmail.com>
2019-05-06 10:13:52 +0200

    Obsluha evenotv vlastnou metódou OnLabelClicl(). Disposing v dizajnéry.

---
+++ Decorator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decorator
+{
+    public partial class Form1 : Form
+    {
+        LabelDecorator _labelDecorator1;
+        LabelDecorator _labelDecorator2;
+        LabelDecorator _labelDecorator3;
+        public Form1()
+        {
+            InitializeComponent();
+            _labelDecorator1 = new LabelDecorator(label1);
+            _labelDecorator2 = new LabelDecorator(label2);
+            _labelDecorator3 = new LabelDecorator(label3);
+        }
+
+        private void Done()
+        {
+            _labelDecorator1.Dispose();
+            _labelDecorator2.Dispose();
+            _labelDecorator3.Dispose();
+        }
+
+        private void label3_Leave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Decorator/LabelDecorator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decorator
+{
+    public class LabelDecorator : IDisposable
+    {
+        private Label _label;
+
+        public LabelDecorator(Label label)
+        {
+            _label = label;
+            AddHandlers();
+        }
+
+        private void AddHandlers()
+        {
+            _label.Click += new System.EventHandler(OnLabelClick);
+            _label.Click += MousePosition;
+        }
+
+        private void MousePosition(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            MouseEventArgs args = (MouseEventArgs)e;
+            label.Text = $"{ args.X }--{ args.Y}";
+        }
+
+        private void RemoveHandlers()
+        {
+            _label.Click -= OnLabelClick;
+            _label.Click -= MousePosition;
+        }
+
+        private void OnLabelClick(object sender, EventArgs e)
+        {
+            _label.BackColor = Color.Aqua;
+        }
+
+
+        public void Dispose()
+        {
+            RemoveHandlers();
+        }
+
+    }
+}
